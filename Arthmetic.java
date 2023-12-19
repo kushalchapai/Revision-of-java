68dce6eaa99e1659d674cbe55d5c3eaebe379ae6
kushal chapai <kushalchapai282@gmail.com>
2023-12-19 12:44:37 +0545

    Question 6 yuifgcdajv

---
+++ Arthmetic.java
@@ -0,0 +1,12 @@
+// Write a java program to print the sum (addition), multiply,subtract,divide and remainder of two numbers.
+//Question 6 (day 2)
+public class Arthmetic {
+    public static void main(String[] args) {
+        int a,b;
+        a= 4;
+        b= 2;
+
+        System.out.println("Addition" + (a+b));
+        
+    }
+}
