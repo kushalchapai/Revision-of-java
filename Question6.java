dca2ae8f6dccd193392cf0cb988bbab067f74d12
kushal chapai <kushalchapai282@gmail.com>
2023-12-19 12:02:37 +0545

    added question 6 not completed

---
+++ Question6.java
@@ -0,0 +1,6 @@
+// Write a java program to print the sum (addition), multiply,subtract,divide and remainder of two numbers.
+public class Question6 {
+    public static void main(String[] args) {
+        
+    }
+}
