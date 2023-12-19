03458c324c9acbaaea79c561842375f75f2beb27
kushal chapai <kushalchapai282@gmail.com>
2023-12-19 12:05:27 +0545

    added addition

--- Question6.java
+++ Question6.java
@@ -1,6 +1,11 @@
 // Write a java program to print the sum (addition), multiply,subtract,divide and remainder of two numbers.
 public class Question6 {
     public static void main(String[] args) {
+        int a,b;
+        a= 4;
+        b= 2;
+
+        System.out.println("Addition" + (a+b));
         
     }
 }
